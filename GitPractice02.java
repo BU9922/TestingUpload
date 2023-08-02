3705fdc585a60155d445e564bfbe0f06d8d63dbd
BU9922 <bunek922@gmail.com>
2023-08-02 18:13:13 +0100

    first commit

---
+++ GitPractice02.java
@@ -0,0 +1,11 @@
+public class GitPractice02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello i am here");
+        System.out.println("how are you");
+        System.out.println("new feature added");
+        System.out.println("new feature in progress");
+
+    }
+}
